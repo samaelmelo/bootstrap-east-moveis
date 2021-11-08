7f6cda84fd0c8d1d9e0e90a316e394f5e7e3afdf
Samael Melo <samaelmelo.dev@gmail.com>
2021-11-08 11:27:50 -0400

    adicionei fotos

---
+++ carrouse.js
@@ -0,0 +1,410 @@
+const kitchen = [
+//   {
+//   id: 0,
+//   title: 'Armário superior',
+//   src: 'https://www.todeschini.com.br/wp-content/uploads/2018/03/COZINHA05_Destaque.jpg'
+// },
+{
+  id: 1,
+  title: 'Armário inferior',
+  src: 'https://www.todeschini.com.br/wp-content/uploads/2020/05/2-1.jpg'
+},
+{
+  id: 2,
+  title: 'Balcão da pia ',
+  src: 'https://www.todeschini.com.br/wp-content/uploads/2020/06/banner-texto-detalhes-encantadores-cozinha-kelya.jpg'
+},
+{
+  id:3,
+  title:'Balcão da pia',
+  src:'https://images.pexels.com/photos/7061399/pexels-photo-7061399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+},
+{
+  id:4,
+  title:'Kit armario Superior e Inferior',
+  src:'https://images.pexels.com/photos/7535014/pexels-photo-7535014.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' 
+},
+]
+const office = [
+ 
+ 
+  {
+    id: 2,
+    title: 'Cadeira de escritório',
+    src:'https://www.todeschini.com.br/wp-content/uploads/2018/05/complem-678x659px.png',
+  },
+  {  
+    id: 3,
+    title: 'Gaveteiro',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1861721-553-553/Rollings-Gaveteiro-Baixo-4gv-Branco-Office.jpg?v=637021053860770000',
+  },
+  {
+    id: 4,
+    title: 'Mesa de escritório',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1819823-553-553/Escrivaninha-120x60-Preto-Compac.jpg?v=637013983534930000',
+  },
+  {
+    id: 5,
+    title: 'Mesa de escritório',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1859644-553-553/Escrivaninha-110x51-C-rodizios-Grafite-grafite-Tweels.jpg?v=637020822772200000',
+  },
+  {
+    id: 6,
+    title: 'Kit Cadeira e mesa',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2452526-553-553/ESCRIVANINHA-120-M-X-60-CM_189887_amb.jpg?v=637465574807170000',
+  },
+  {
+    id: 7,
+    title: 'Kit Cadeira e mesa',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2452506-553-553/ESCRIVANINHA-110-M-X-51-CM-C-ROD-ZIOS_189885_amb.jpg?v=637465572751870000',
+  }
+]
+
+const doubleRoom = [
+  
+  {
+    id:1,
+    title: 'Berço',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1757464-553-553/Berco-68-Branco-natural-Washed-Crescemais.jpg?v=637002624242070000'
+  },
+  {
+    id:2,
+    title: 'Cama box de casal',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1576723-553-553/maxbc138gvds13818819_brd33.jpg?v=636961152137870000'
+  },
+  {
+    id:3,
+    title: 'Cama de casal sem cabiceira',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2355990-553-553/cama-casal-138-branco-nozes-lin_st0.jpg?v=637432733321400000'
+  },
+  {
+    id:4,
+    title: 'Cama de casal sem cabiceira',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2356129-553-553/cama-casal-138-preto-nozes-lin_st0.jpg?v=637432738242370000'
+  },
+  {
+    id:5,
+    title: 'Cama de casal com cabiceira',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/89183-29124Q_685x712px.jpg'
+  },
+  {
+    id:6,
+    title: 'Cama de casal com cabiceira',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/770d3-29756K.jpg'
+  },
+  {
+    id:7,
+    title: 'Criado Mudo',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2750605-553-553/mesa-de-cabeceira-3-gavetas-bege-nozes-lin_st0.jpg?v=637588558671770000'
+  },
+  {
+    id:8,
+    title: 'Criado Mudo',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1859498-553-553/Criado-mudo-3gv-Preto-nozes-Lin.jpg?v=637020807844200000'
+  },
+  {
+    id:9,
+    title: 'Guarda roupas 3 portas',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1631702-553-553/Estrutura-Guarda-roupa-3-Portas-Freijo-Friz.jpg?v=636972639252570000'
+  },
+  {
+    id:10,
+    title: 'Guarda roupas 3 portas',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1843185-553-553/Estrutura-Guarda-roupa-3-Portas-Preto-Slides.jpg?v=637018449232900000'
+  },
+  {
+    id:11,
+    title: 'Guarda roupas 4 portas',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1861430-553-553/Guarda-roupa-4-Portas-3gv-35-Branco-Maximus.jpg?v=637021004644570000'
+  }
+]
+
+const singleRoom = [
+ 
+  {
+    id: 1,
+    title:'Beliche',
+    src: 'https://tokstok.vtexassets.com/arquivos/ids/1576466-553-553/teenbelteensci78_ad.jpg?v=636961143695470000'
+  },
+  {
+    id: 2,
+    title:'Cama box de solteiro',
+    src: 'https://tokstok.vtexassets.com/arquivos/ids/2280761-553-553/base-box-solteiro-98-corsin-preto-max_st0.jpg?v=637298804056900000'
+  },
+  {
+    id: 3,
+    title:'Cama de solteiro com cabiceira',
+    src: 'https://tokstok.vtexassets.com/arquivos/ids/1859820-553-553/Cama-Solteiro-98-Preto-Skand.jpg?v=637020849260900000'
+  },
+  {
+    id: 4,
+    title:'Cama de solteiro sem cabiceira',
+    src: 'https://tokstok.vtexassets.com/arquivos/ids/1659732-553-553/Cama-Solteiro-2gv-88-Castanho-Crates.jpg?v=636977515733130000'
+  },
+  {
+    id: 5,
+    title:'Cama de solteiro sem cabiceira',
+    src: 'https://tokstok.vtexassets.com/arquivos/ids/2355728-553-553/cama-solteiro-88-preto-nozes-lin_st0.jpg?v=637432723388830000'
+  },
+  {
+    id:6,
+    title: 'Criado Mudo',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2750605-553-553/mesa-de-cabeceira-3-gavetas-bege-nozes-lin_st0.jpg?v=637588558671770000'
+  },
+  {
+    id:7,
+    title: 'Criado Mudo',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1859498-553-553/Criado-mudo-3gv-Preto-nozes-Lin.jpg?v=637020807844200000'
+  },
+  {
+    id:8,
+    title:'Guarda roupa 2 portas',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1843119-553-553/Estrutura-Guarda-roupa-2-Portas-Preto-Slides.jpg?v=637018448498800000'
+
+  },
+  {
+    id:9,
+    title: 'Guarda roupa 3 portas',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1631702-553-553/Estrutura-Guarda-roupa-3-Portas-Freijo-Friz.jpg?v=636972639252570000'
+  },
+  {
+    id:10,
+    title: 'Guarda roupa 3 portas',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1843185-553-553/Estrutura-Guarda-roupa-3-Portas-Preto-Slides.jpg?v=637018449232900000'
+  },
+  {
+    id:11,
+    title: 'Painel',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/0b211-29440.jpg'
+  },
+  {
+    id:12,
+    title: 'Painel',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2333369-553-553/painel-tv-120-c-prateleira-preto-channels_st0.jpg?v=637378291385800000'
+  },
+  {
+    id: 13,
+    title: 'Painel',
+    src: 'https://tokstok.vtexassets.com/arquivos/ids/2139845-553-553/painel-tv-136-natural-washed-natural-washed-cell_st0.jpg?v=637164022356170000'
+  },
+  {
+    id: 14,
+    title: 'Rack',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/2042c-21c47-29435.jpg'
+  },
+  {
+    id:15,
+    title: 'Kit rack e painel',
+    src:'https://staticmobly.akamaized.net/p/Imcal-Rack-Classic-II-Off-White-e-Nature-52x218-cm-1911-933067-1-zoom.jpg'
+  },
+  {
+    id:16,
+    title: 'Kit rack e painel',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/30386-ambiente.jpg'
+  },
+  {
+    id:17,
+    title: 'Kit rack e painel',
+    src:'https://madesacms.vteximg.com.br/arquivos/ids/205857-1000-1000/02-XA70035Z1-ambientado.jpg?v=637413150788700000'
+  },
+]
+
+const livingRoom = [
+ 
+  {
+    id:1,
+    title: 'Aparador',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/61a0e-25936N.jpg'
+  },
+  {
+    id:2,
+    title: 'Aparador',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/462ab-2071BR_.jpg'
+  },
+  {
+    id:3,
+    title: 'Escrivaninha',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/462ab-2071BR_.jpg'
+  },
+  {
+    id:4,
+    title: 'Escrivaninha',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/b5105-2025.jpg'
+  },
+  {
+    id:5,
+    title: 'Painel',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/0b211-29440.jpg'
+  },
+  {
+    id:6,
+    title: 'Painel',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/2333369-553-553/painel-tv-120-c-prateleira-preto-channels_st0.jpg?v=637378291385800000'
+  },
+  {
+    id: 7,
+    title: 'Painel',
+    src: 'https://tokstok.vtexassets.com/arquivos/ids/2139845-553-553/painel-tv-136-natural-washed-natural-washed-cell_st0.jpg?v=637164022356170000'
+  },
+  {
+    id: 8,
+    title: 'Rack',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/2042c-21c47-29435.jpg'
+  },
+  {
+    id:9,
+    title: 'Kit rack e painel',
+    src:'https://staticmobly.akamaized.net/p/Imcal-Rack-Classic-II-Off-White-e-Nature-52x218-cm-1911-933067-1-zoom.jpg'
+  },
+  {
+    id:10,
+    title: 'Kit rack e painel',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/30386-ambiente.jpg'
+  },
+  {
+    id:11,
+    title: 'Kit rack e painel',
+    src:'https://madesacms.vteximg.com.br/arquivos/ids/205857-1000-1000/02-XA70035Z1-ambientado.jpg?v=637413150788700000'
+  },
+  {
+    id:12,
+    title: 'Sofá 2 lugares',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/66dac-27232_685x712px.JPG',
+  },
+  {
+    id:13,
+    title: 'Sofá 2 lugares',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/b2b74-27182_685x712px.JPG'
+  },
+  {
+    id: 14,
+    title: 'Sofá 3 lugares',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/e0001-22803-A.jpg',
+  },
+  {
+    id: 15,
+    title: 'Poltrona',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/727a8-cadeira-2-onix.jpg',
+  },
+  {
+    id: 16,
+    title: 'Poltrona',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/2294b-2225_1.jpg',
+  },
+  {
+    id: 17,
+    title: 'Mesa de centro',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/8fcf4-Modulo-Horus-sem-braco.jpg',
+  },
+  {
+    id: 18,
+    title: 'Mesa de centro',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/c79b1-Mesa-Apoio-Ametista.jpg',
+  },
+
+
+]
+
+const dinnigRoom = [
+ 
+  {
+    id:1,
+    title: 'Cadeira para mesa de jantar',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/4b369-2060.jpg'
+  },
+  {
+    id:2,
+    title: 'Mesa de jantar e duas cadeiras',
+    src:'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/db45e-Mesa-de-Jantar-Suez.jpg'
+  },
+  {
+    id:3,
+    title: 'Mesa de jantar quatro cadeiras',
+    src:'https://tokstok.vtexassets.com/arquivos/ids/1577915-553-553/metbmtm13075toujcd_pt3.jpg?v=636961205678000000',
+  },
+  {
+    id:4,
+    title: 'Mesa de jantar e cinco cadeiras',
+    src: 'https://sierra.com.br/assets/uploads/wt_site_produtos_imagens/interna/3b9b6-Mesa-de-Jantar-Egeu.jpg',
+
+  }
+]
+
+
+let carrousel = document.querySelector('.carousel-inner')
+
+kitchen.map( item => {
+  carrousel.innerHTML += `
+  
+     <div class="carousel-item">
+        <span>${item.title}</span>
+        <img src="${item.src}" class="d-block w-100" alt="...">
+      </div> 
+  
+      `
+})
+
+
+
+let carrouselOffice = document.querySelector('.office')
+office.map( item => {
+  carrouselOffice.innerHTML += `
+  
+  <div class="carousel-item">
+      <span>${item.title}</span>
+      <img src="${item.src}" class="d-block w-100" alt="...">
+  </div> 
+  
+  `
+})
+
+
+let carrouselDoubleRoom = document.querySelector('.double-room')
+doubleRoom.map( item => {
+  carrouselDoubleRoom.innerHTML += `
+  
+  <div class="carousel-item">
+      <span>${item.title}</span>
+      <img src="${item.src}" class="d-block w-100" alt="...">
+  </div> 
+  
+  `
+})
+
+
+let carrouselSingleRoom = document.querySelector('.single-room')
+singleRoom.map( item => {
+  carrouselSingleRoom.innerHTML += `
+  
+  <div class="carousel-item">
+      <span>${item.title}</span>
+      <img src="${item.src}" class="d-block w-100" alt="...">
+  </div> 
+  
+  `
+})
+
+let carrouselLivingRoom = document.querySelector('.living-room')
+livingRoom.map( item => {
+  carrouselLivingRoom.innerHTML += `
+  
+  <div class="carousel-item">
+      <span>${item.title}</span>
+      <img src="${item.src}" class="d-block w-100" alt="...">
+  </div> 
+  
+  `
+})
+
+let carrouselDinnigRoom = document.querySelector('.dinning-room')
+dinnigRoom.map( item => {
+  carrouselDinnigRoom.innerHTML += `
+  
+  <div class="carousel-item">
+      <span>${item.title}</span>
+      <img src="${item.src}" class="d-block w-100" alt="...">
+  </div> 
+  
+  `
+})
+
